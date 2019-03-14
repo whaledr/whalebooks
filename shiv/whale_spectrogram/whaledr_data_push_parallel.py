007479f6e6206f639eeb3da01e107446927812cb
Shivraj <shivrajpurshottam@gmail.com>
2019-03-14 12:43:17 -0700

    Temporal component of spectogram changed from 5 to 10 sec

--- shiv/whale_spectrogram/whaledr_data_push_parallel.py
+++ shiv/whale_spectrogram/whaledr_data_push_parallel.py
@@ -111,7 +111,7 @@ def data_push(data_url):
             nfft = int(_nearest_pow_2(wlen * samp_rate))  # number of fft points of each bin
             per_lap = 0.995      # percentage of overlap
             nlap = int(nfft * float(per_lap))   # number of overlapped samples
-            timestep = 5  # save results every 5 seceonds (no overlap)
+            timestep = 10  # save results every 5 seceonds (no overlap)
 
             for i in range(0, len(pingtimes), timestep):
                 st = stream.slice(UTCDateTime(pingtimes[i]), UTCDateTime(pingtimes[i]) + step_size)
