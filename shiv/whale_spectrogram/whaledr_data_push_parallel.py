6d148a401ff12dcab821f103dccbdd5b969065ce
Ubuntu <ubuntu@ip-172-31-21-101.us-west-2.compute.internal>
2019-05-02 08:55:30 +0000

    Fix tick labels

--- shiv/whale_spectrogram/whaledr_data_push_parallel.py
+++ shiv/whale_spectrogram/whaledr_data_push_parallel.py
@@ -23,7 +23,7 @@ from multiprocessing import Pool
 import json
 import gc
 
-logging.basicConfig(filename='whaledr_data_upload.log', level=logging.INFO)
+logging.basicConfig(filename='whaledr_data_upload.log', level=logging.INFO, filemode="w")
 
 
 bucket_name = 'whaledr'
@@ -145,15 +145,20 @@ def data_push(data_url):
                 ax = plt.Axes(fig, [0., 0., 1., 1.])
                 ax.set_axis_off()
                 fig.add_axes(ax)
+                freq_ticks = [0, 4, 8]
+                time_ticks = [0, 5, 10]
+
                 cax = ax.imshow(specgram, interpolation="nearest", extent=extent, norm=norm, cmap='bone')
                 dpi = fig.get_dpi()
                 fig.set_size_inches(512/float(dpi), 512/float(dpi))
-                ax.axis('tight')
                 ax.set_xlim(0, end)
                 ax.set_ylim(0.0, 8.)
                 ax.grid(False)
                 ax.set_xlabel('Time [s]')
                 ax.set_ylabel('Frequency [kHz]')
+                ax.set_xticks(time_ticks)
+                ax.set_yticks(freq_ticks)
+
                 filename = st[0].stats.network+'_' + \
                            st[0].stats.station+'_' + \
                            st[0].stats.location+'_' + \
@@ -166,6 +171,7 @@ def data_push(data_url):
                                    '{}/{}/{}/'.format(folder_name, hydrophone_name, url_date) + \
                                        filename[:-8] + '.jpg')
                 os.remove(filename[:-8] + '.jpg')
+                print(filename)
                 plt.cla()
                 plt.clf()
                 plt.close('all')
@@ -230,7 +236,6 @@ if __name__ == '__main__':
     mainurl = 'https://rawdata.oceanobservatories.org/files/CE04OSBP/LJ01C/11-HYDBBA105/2019/01/14/'
     
     url_list = get_data_url_list(mainurl)
-    
     try:
         nCores = 12
         
